f432a6f817b7522998c31a0be0647a2b72a06032
SaiVasudev10 <vasudev.gandham@gmail.com>
2026-07-15 14:35:32 +0530

    hello world

---
+++ 3-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+
+echo "$PERSON1: How are you?"
+echo "$PERSON2: I'm doing good Thank you!! How about you?"
+echo "$PERSON1: I'm good!!! Thank you."
+echo "$PERSON2: I was great talking to you."
